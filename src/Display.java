5649604a4c70d30ab55330eb653368cb772e07df
AlexP <lexanorilsk@gmail.com>
2024-07-15 02:04:51 +0300

    Add Display and make JPanel

---
+++ src/Display.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Display {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello world");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(720, 480);
+        frame.setResizable(false);
+        frame.add(new Main(frame));
+        frame.setVisible(true);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JPanel implements ActionListener {
+    JFrame frame;
+
+    public Main(JFrame frame) {
+        this.frame = frame;
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(20, 20, 100, 100);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
     }
 }
